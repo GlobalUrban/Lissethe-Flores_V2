0d339cb38f21277099080ee5bb5272a5c68aa4cd
GlobalUrban <112003685+GlobalUrban@users.noreply.github.com>
2022-08-26 14:29:11 -0600

    LIsseth FLores V2 Started

---
+++ nav.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.getElementsByClassName("nav-links__container-a");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
+
+
+for(let i = 0; i < 4; i++){
+  links[i].addEventListener("click", () =>{
+    navLinks.classList.remove("open")
+    console.log("se removio el open");
+  });
+}
